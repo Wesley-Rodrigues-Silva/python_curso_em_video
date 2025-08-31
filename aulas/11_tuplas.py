df6d2ab5191f203dac1202fa499d034891b2ed9d
wesley-rodrigues-silva <161177795+Wesley-Rodrigues-Silva@users.noreply.github.com>
2025-08-31 16:44:45 -0300

    att

---
+++ aulas/11_tuplas.py
@@ -0,0 +1,7 @@
+lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+for c in lanche:
+    if c == 'Suco':
+        print('Eu vou beber Suco')
+    else:
+        print(f'Eu vou comer {c}')
+print('Comi pra caramba')
