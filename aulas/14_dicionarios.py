6ff86bdf6d09b0b2da94847480ebfb62dc03cea0
wesley-rodrigues-silva <161177795+Wesley-Rodrigues-Silva@users.noreply.github.com>
2025-09-04 18:43:27 -0300

    att

---
+++ aulas/14_dicionarios.py
@@ -0,0 +1,13 @@
+'''pessoas = {
+    'nome': 'Gustavo', 'sexo': 'M', 'idade': 22
+}
+pessoas ['peso'] = 98.5
+for k, v in pessoas.items():
+    print(f'{k} = {v}')'''
+
+brasil = []
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+print(brasil)
