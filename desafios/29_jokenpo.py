689538af67d29d5a8b9f2b96fbffa5a4e39a82f6
wesley-rodrigues-silva <161177795+Wesley-Rodrigues-Silva@users.noreply.github.com>
2025-09-06 09:37:28 -0300

    att

--- desafios/29_jokenpo.py
+++ desafios/29_jokenpo.py
@@ -1,6 +1,45 @@
-import random
-pedra = 0
-papel = 1
-tesoura = 2
-print('Suas opções:\n[ 0 ] PEDRA\n[ 1 ] PAPEL\n[ 2 ] TESOURA')
+from random import randint
+from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+print('''Suas opções:
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA''')
 jogador = int(input('Qual é sua jogada? '))
+print('JO')
+sleep(0.7)
+print('KEN')
+sleep(0.7)
+print('PO!!!')
+print('-=' * 11)
+print(f'Computador jogou {itens[computador]}')
+print(f'Você jogou {itens[jogador]}')
+print('-=' * 11)
+if computador == 0:
+    if jogador == 0:
+        print('EMPATE! Os dois fizeram a mesma jogada')
+    elif jogador == 1:
+        print('Você venceu!')
+    elif jogador == 2:
+        print('O computador venceu!')
+    else:
+        print('Jogada inválida!')
+if computador == 1:
+    if jogador == 0:
+        print('O computador venceu!')
+    elif jogador == 1:
+        print('EMPATE! Os dois fizeram a mesma jogada')
+    elif jogador == 2:
+        print('Você venceu!')
+    else:
+        print('Jogada inválida!')
+if computador == 2:
+    if jogador == 0:
+        print('Você venceu!')
+    elif jogador == 1:
+        print('O computador venceu!')
+    elif jogador == 2:
+        print('EMPATE! Os dois fizeram a mesma jogada')
+    else:
+        print('Jogada inválida!')
